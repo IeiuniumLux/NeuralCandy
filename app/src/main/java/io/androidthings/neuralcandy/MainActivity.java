c4a051e13908368f08922e1e9ebdf42104fa4aaf
abencomo <cagiva851@gmail.com>
2018-02-18 00:12:28 -0800

    Add TensorFlow Lite support

--- app/src/main/java/io/androidthings/neuralcandy/MainActivity.java
+++ app/src/main/java/io/androidthings/neuralcandy/MainActivity.java
@@ -1,24 +1,28 @@
 package io.androidthings.neuralcandy;
 
 import android.app.Activity;
+import android.graphics.Bitmap;
 import android.graphics.drawable.AnimationDrawable;
+import android.media.ImageReader;
 import android.os.Bundle;
 import android.os.CountDownTimer;
 import android.util.Log;
-import android.view.LayoutInflater;
 import android.view.MotionEvent;
 import android.view.View;
-import android.widget.CompoundButton;
 import android.widget.ImageView;
-import android.widget.LinearLayout;
-import android.widget.Switch;
 import android.widget.TextView;
 
 import com.google.android.things.pio.Gpio;
 import com.google.android.things.pio.PeripheralManagerService;
 
+import org.tensorflow.lite.Interpreter;
+
 import java.io.IOException;
+import java.nio.ByteBuffer;
+import java.nio.ByteOrder;
 import java.util.Arrays;
+import java.util.Collection;
+import java.util.Iterator;
 import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
@@ -41,8 +45,28 @@ public class MainActivity extends Activity {
 
     private static final String TAG = MainActivity.class.getSimpleName();
 
-    AnimationDrawable processingAnimation;
-    TextView textInfo;
+    /** Camera image capture size */
+    private static final int PREVIEW_IMAGE_WIDTH = 640;
+    private static final int PREVIEW_IMAGE_HEIGHT = 480;
+    /** Image dimensions required by TF model */
+    private static final int TF_INPUT_IMAGE_WIDTH = 224;
+    private static final int TF_INPUT_IMAGE_HEIGHT = 224;
+    /** Dimensions of model inputs. */
+    private static final int DIM_BATCH_SIZE = 1;
+    private static final int DIM_PIXEL_SIZE = 3;
+    /** TF model asset files */
+    private static final String LABELS_FILE = "labels.txt";
+    private static final String MODEL_FILE = "mobilenet_quant_v1_224.tflite";
+
+
+    private AnimationDrawable mAnimation;
+    private ImageView mImgView;
+    private TextView mTextInfo;
+    private boolean mProcessing;
+    private Interpreter mTensorFlowLite;
+    private List<String> mLabels;
+    private CameraHandler mCameraHandler;
+    private ImagePreprocessor mImagePreprocessor;
 
     public static final List<String> LABELS = Arrays.asList("DOG",
             "CAT","FISH","SHEEP","COW","BEE","LION","PENGUIN","BIRD",
@@ -55,10 +79,10 @@ public class MainActivity extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        ImageView imgView = (ImageView) findViewById(R.id.image);
-        textInfo = (TextView) findViewById(R.id.textInfo);
+        mImgView = (ImageView) findViewById(R.id.image);
+        mTextInfo = (TextView) findViewById(R.id.textInfo);
 
-        imgView.setOnTouchListener(new View.OnTouchListener() {
+        mImgView.setOnTouchListener(new View.OnTouchListener() {
             @Override
             public boolean onTouch(View view, MotionEvent event) {
                 final ImageView img = (ImageView) view;
@@ -66,22 +90,22 @@ public class MainActivity extends Activity {
                     if (img.getDrawable().getConstantState().equals
                             (img.getContext().getDrawable(R.drawable.ic_start).getConstantState())) {
                         img.setImageResource(R.drawable.ic_camera);
-                        textInfo.setText("Processing...");
+                        mTextInfo.setText("Processing...");
                     } else if (img.getDrawable().getConstantState().equals
                             (img.getContext().getDrawable(R.drawable.ic_camera).getConstantState())) {
                         img.setImageResource(R.drawable.animation);
-                        processingAnimation = (AnimationDrawable) img.getDrawable();
-                        processingAnimation.start();
+                        mAnimation = (AnimationDrawable) img.getDrawable();
+                        mAnimation.start();
                         new CountDownTimer(5000, 1000) {
 
                             public void onTick(long millisUntilFinished) {
-                                textInfo.setText("Analyzing... " + millisUntilFinished / 1000);
+                                mTextInfo.setText("Analyzing... " + millisUntilFinished / 1000);
                             }
 
                             public void onFinish() {
-                                processingAnimation.stop();
+                                mAnimation.stop();
                                 img.setImageResource(R.drawable.ic_thumb_up);
-                                textInfo.setText("done!");
+                                mTextInfo.setText("done!");
                             }
                         }.start();
                     }
@@ -141,5 +165,135 @@ public class MainActivity extends Activity {
             }
         }
         mGpioMap.clear();
+    }
+
+    private void updateStatus(String status) {
+        Log.d(TAG, status);
+        mTextInfo.setText(status);
+    }
+
+    /**
+     * Initialize the classifier that will be used to process images.
+     */
+    private void initClassifier() {
+        try {
+            mTensorFlowLite = new Interpreter(TensorFlowHelper.loadModelFile(this, MODEL_FILE));
+            mLabels = TensorFlowHelper.readLabels(this, LABELS_FILE);
+        } catch (IOException e) {
+            Log.w(TAG, "Unable to initialize TensorFlow Lite.", e);
+        }
+    }
+
+    /**
+     * Clean up the resources used by the classifier.
+     */
+    private void destroyClassifier() {
+        mTensorFlowLite.close();
+    }
+
+    /**
+     * Image capture process complete
+     */
+    private void onPhotoReady(Bitmap bitmap) {
+        mImgView.setImageBitmap(bitmap);
+        doRecognize(bitmap);
+    }
+
+    /**
+     * Image classification process complete
+     */
+    private void onPhotoRecognitionReady(Collection<Recognition> results) {
+        updateStatus(formatResults(results));
+        mProcessing = false;
+    }
+
+    /**
+     * Format results list for display
+     */
+    private String formatResults(Collection<Recognition> results) {
+        if (results == null || results.isEmpty()) {
+            return getString(R.string.empty_result);
+        } else {
+            StringBuilder sb = new StringBuilder();
+            Iterator<Recognition> it = results.iterator();
+            int counter = 0;
+            while (it.hasNext()) {
+                Recognition r = it.next();
+                sb.append(r.getTitle());
+                counter++;
+                if (counter < results.size() - 1) {
+                    sb.append(", ");
+                } else if (counter == results.size() - 1) {
+                    sb.append(" or ");
+                }
+            }
+
+            return sb.toString();
+        }
+    }
+
+    /**
+     * Process an image and identify what is in it. When done, the method
+     * {@link #onPhotoRecognitionReady(Collection)} must be called with the results of
+     * the image recognition process.
+     *
+     * @param image Bitmap containing the image to be classified. The image can be
+     *              of any size, but preprocessing might occur to resize it to the
+     *              format expected by the classification process, which can be time
+     *              and power consuming.
+     */
+    private void doRecognize(Bitmap image) {
+        // Allocate space for the inference results
+        byte[][] confidencePerLabel = new byte[1][mLabels.size()];
+        // Allocate buffer for image pixels.
+        int[] intValues = new int[TF_INPUT_IMAGE_WIDTH * TF_INPUT_IMAGE_HEIGHT];
+        ByteBuffer imgData = ByteBuffer.allocateDirect(
+                DIM_BATCH_SIZE * TF_INPUT_IMAGE_WIDTH * TF_INPUT_IMAGE_HEIGHT * DIM_PIXEL_SIZE);
+        imgData.order(ByteOrder.nativeOrder());
+
+        // Read image data into buffer formatted for the TensorFlow model
+        TensorFlowHelper.convertBitmapToByteBuffer(image, intValues, imgData);
+
+        // Run inference on the network with the image bytes in imgData as input,
+        // storing results on the confidencePerLabel array.
+        mTensorFlowLite.run(imgData, confidencePerLabel);
+
+        // Get the results with the highest confidence and map them to their labels
+        Collection<Recognition> results = TensorFlowHelper.getBestResults(confidencePerLabel, mLabels);
+        // Report the results with the highest confidence
+        onPhotoRecognitionReady(results);
+    }
+
+    /**
+     * Initialize the camera that will be used to capture images.
+     */
+    private void initCamera() {
+        mImagePreprocessor = new ImagePreprocessor(PREVIEW_IMAGE_WIDTH, PREVIEW_IMAGE_HEIGHT,
+                TF_INPUT_IMAGE_WIDTH, TF_INPUT_IMAGE_HEIGHT);
+        mCameraHandler = CameraHandler.getInstance();
+        mCameraHandler.initializeCamera(this,
+                PREVIEW_IMAGE_WIDTH, PREVIEW_IMAGE_HEIGHT, null,
+                new ImageReader.OnImageAvailableListener() {
+                    @Override
+                    public void onImageAvailable(ImageReader imageReader) {
+                        Bitmap bitmap = mImagePreprocessor.preprocessImage(imageReader.acquireNextImage());
+                        onPhotoReady(bitmap);
+                    }
+                });
+    }
+
+    /**
+     * Clean up resources used by the camera.
+     */
+    private void closeCamera() {
+        mCameraHandler.shutDown();
+    }
+
+    /**
+     * Load the image that will be used in the classification process.
+     * When done, the method {@link #onPhotoReady(Bitmap)} must be called with the image.
+     */
+    private void loadPhoto() {
+        mCameraHandler.takePicture();
     }
 }
